d3e42e3918c5ff883231f7d6839bcb53e37ce1c8
VASISHTA MAHI TEJ D <vasistadronadula@gmail.com>
2026-08-13 15:37:25 +0530

    feat(pos): update POS system and clean up deprecated components

--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -2,13 +2,12 @@
 
 import React from 'react';
 import AdminGuard from '@/components/admin/AdminGuard';
-import POSClientWrapper from '@/components/pos/POSClientWrapper';
-import { initialMenuItems } from '@/data/posMenuData';
+import CounterBillingPage from '@/app/admin/pos/page';
 
 export default function CashierPage() {
   return (
     <AdminGuard>
-      <POSClientWrapper initialMenuItems={initialMenuItems} />
+      <CounterBillingPage />
     </AdminGuard>
   );
 }
